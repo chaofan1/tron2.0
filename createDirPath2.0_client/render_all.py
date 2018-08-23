db7af8fca30bdcbcf19e30c5486211f08439aa50
Alaskaka <wangchaofan1991@gmail.com>
2018-08-23 12:25:29 +0800

    Merge branch 'master' of https://github.com/chaofan1/tron2.0
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- createDirPath2.0_client/render_all.py
+++ createDirPath2.0_client/render_all.py
@@ -9,7 +9,7 @@ def render_all(inPathFile):
     app.setStyleSheet('QMainWindow {border:0px solid black;background:rgb(255, 255, 255)}')
     mainWindow = QMainWindow()
     mainWindow.setWindowFlags(Qt.WindowStaysOnTopHint)
-    dir_path = QFileDialog.getExistingDirectory(mainWindow, "选择您的路径",inPathFile)
+    dir_path = QFileDialog.getExistingDirectory(mainWindow, "选择您的路径",inPathFile,options=QFileDialog.ReadOnly)
     return dir_path
     sys.exit(app.exec_())
 
