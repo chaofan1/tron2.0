989700afa92dd300edde6b80a468e8a73cff3679
lx <1120722567@qq.com>
2018-09-14 18:55:40 +0800

    修改外包分发 lx

--- createDirPath2.0_server/distribution.py
+++ createDirPath2.0_server/distribution.py
@@ -18,11 +18,11 @@ class Finish(SyntaxWarning):
 
 class TronDistribute:
     def __init__(self, command_id):
-        self.command_id = command_id
-        self.material = 'material'
-        self.asset = 'asset'
-        self.references = 'references'
-        self.compressType = 'zip'  # tar,bztar,gzrar
+        # self.command_id = command_id
+        # self.material = 'material'
+        # self.asset = 'asset'
+        # self.references = 'references'
+        # self.compressType = 'zip'  # tar,bztar,gzrar
         self.pool = threadpool.ThreadPool(8)
         self.rpath = os.getcwd()
         if platform.system() == 'Windows':
@@ -41,29 +41,38 @@ class TronDistribute:
             Paths = []
             # 解析参数
             self.task_ids = response['task_ids']
-            self.cnameList = response['company_data']
+            self.cpname = response['company_data']['dir_name']
+            self.email = response['company_data']['email']
+            self.user_name = response['user_name']
+            self.user_secret = response['user_name']
+            self.user_mail = response['user_mail']
+            self.describe = response['describe']
+
             fieldList = response['field_data']
+            assetList = response['assets']
+            referencesList = response['references']
             for field in fieldList:
                 fieldName = field['field_name']
                 shotList = field['shot_data']
                 for shot in shotList:
                     shotNum = shot['shot_number']
                     material = shot['material']
-                    assetList = shot['asset_data']
-                    referencesList = shot['references_data']
-                    for cp in self.cnameList:
-                        cp = cp['name'] + '_' + self.userid + '_' + self.timeStamp
-                        basePath = self.outputPath + os.sep + cp['name'] + os.sep + cp + os.sep + self.progectName + os.sep + fieldName + os.sep + shotNum + os.sep
-                        materialDir = basePath + self.material
-                        assetDir = basePath + self.asset
-                        referencesDir = basePath + self.references
 
-                        os.makedirs(materialDir)
-                        os.makedirs(assetDir)
-                        os.makedirs(referencesDir)
-                        Paths.append(((materialDir, material), None))
-                        Paths.append(((assetDir, assetList), None))
-                        Paths.append(((referencesDir, referencesList), None))
+                    self.cp = self.cpname + '_' + self.userid + '_' + self.timeStamp
+                    basePath = self.outputPath + os.sep + self.cpname + os.sep + self.progectName + \
+                               os.sep + fieldName + os.sep + shotNum + os.sep
+                    os.makedirs(basePath)
+                    Paths.append(((basePath, material), None))
+
+            assetDir = self.outputPath + os.sep + self.cpname + os.sep + self.progectName + os.sep + \
+                       "assets"
+            referencesDir = self.outputPath + os.sep + self.cpname + os.sep + self.progectName + \
+                            os.sep + "references"
+            os.makedirs(assetDir)
+            os.makedirs(referencesDir)
+            Paths.append(((assetDir, assetList), None))
+            Paths.append(((referencesDir, referencesList), None))
+
             logging.info('打开文件，解析参数成功' + self.filePath )
             self.putThread('copy', Paths)
         except Exception as e:
@@ -77,28 +86,30 @@ class TronDistribute:
             [self.pool.putRequest(req) for req in requests]
             try:
                 self.pool.wait()
-                self.putThread('pack')
+                os.remove(self.filePath)
+                self.putThread('transit')
             except Exception as e:
                 pass
 
-        elif task == 'pack':
-            print('---开始压缩---')
-            requests = threadpool.makeRequests(self.packFile, self.cnameList, callback=self.result)
-            [self.pool.putRequest(req) for req in requests]
-            try:
-                self.pool.wait()
-                # self.putThread('transit')  用云之后打开
-                os.remove(self.filePath)
-            except Exception as e:
-                pass
+        # elif task == 'pack':
+        #     print('---开始压缩---')
+        #     requests = threadpool.makeRequests(self.packFile, callback=self.result)
+        #     [self.pool.putRequest(req) for req in requests]
+        #     try:
+        #         self.pool.wait()
+        #         # self.putThread('transit')  用云之后打开
+        #         os.remove(self.filePath)
+        #     except Exception as e:
+        #         pass
 
         elif task == 'transit':
             print('---开始上传云---')
-            requests = threadpool.makeRequests(self.transitYun, self.cnameList, callback=self.result)
+            requests = threadpool.makeRequests(self.transitYun, callback=self.result)
             [self.pool.putRequest(req) for req in requests]
             try:
                 self.pool.wait()
-                os.remove(self.filePath)
+                self.sendMail(self.cpname, self.email)
+                shutil.rmtree(self.outputPath + os.sep + self.cpname + os.sep + self.progectName)
             except Exception as e:
                 pass
 
@@ -125,79 +136,72 @@ class TronDistribute:
             logging.error(e)
             return 1, e
 
-    def packFile(self, cpName):
-        os.chdir(self.outputPath)
+    # def packFile(self):
+    #     os.chdir(self.outputPath)
+    #     try:
+    #         cpdirPath = self.cpname + '_' + self.userid + '_' + self.timeStamp
+    #         fileList = []
+    #         for root, dirs, files in os.walk(cpdirPath):
+    #             for name in files:
+    #                 fileList.append(os.path.join(root, name))
+    #
+    #         zf = zipfile.ZipFile(cpdirPath + '.zip', "w", zipfile.ZIP_STORED, allowZip64=True)
+    #         for tar in fileList:
+    #             zf.write(tar, tar.lstrip(cpdirPath + os.sep))
+    #         zf.close()
+    #         # 压缩成功，删除公司文件夹
+    #         shutil.rmtree(self.outputPath + os.sep + cpName['name'] + os.sep + cpName['name'] + '_' + self.userid + '_' + self.timeStamp)
+    #         logging.info(u'压缩文件成功' + cpdirPath )
+    #         self.sendMail(cpName)   # 上线之后放传云函数里
+    #         return 0, None
+    #     except Exception as e:
+    #         logging.info(u'压缩出错')
+    #         logging.error(e)
+    #         return 1, e
+
+    def transitYun(self):
         try:
-            cpdirPath = cpName['name'] + '_' + self.userid + '_' + self.timeStamp
-            fileList = []
-            for root, dirs, files in os.walk(cpdirPath):
-                for name in files:
-                    fileList.append(os.path.join(root, name))
-
-            zf = zipfile.ZipFile(cpdirPath + '.zip', "w", zipfile.ZIP_STORED, allowZip64=True)
-            for tar in fileList:
-                zf.write(tar, tar.lstrip(cpdirPath + os.sep))
-            zf.close()
-            # 压缩成功，删除公司文件夹
-            shutil.rmtree(self.outputPath + os.sep + cpName['name'] + os.sep + cpName['name'] + '_' + self.userid + '_' + self.timeStamp)
-            logging.info(u'压缩文件成功' + cpdirPath )
-            self.sendMail(cpName)   # 上线之后放传云函数里
-            return 0, None
-        except Exception as e:
-            logging.info(u'压缩出错')
-            logging.error(e)
-            return 1, e
-
-    def transitYun(self, cpName):
-        try:
-            cpdirPath = cpName['name'] + '_' + self.userid + '_' + self.timeStamp + '.zip'
-            s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
-            if 'Distribute' not in s3.buckets.all():
-                s3.create_bucket(Bucket='Distribute')
-
-            s3.Object("Distribute", cpName + '_' + self.userid + '_' + self.timeStamp).upload_file(cpdirPath)
-            # 成功之后删除压缩包
-            os.remove(cpdirPath)
-            logging.info('上传云成功' + cpdirPath )
+            cpdirPath = self.outputPath + os.sep + self.cpname + os.sep + self.cp
+            #写上传云代码
             return 0, None
         except Exception as e:
             logging.info('上传云出错')
             logging.error(e)
             return 1, e
 
-    def sendMail(self, mailInfo):
-        if mailInfo['email']:
+    def sendMail(self, name, email):
+        if email:
             try:
                 smtp_server = 'smtp.163.com'
-                from_mail = '15810448048@163.com'  # 发送邮箱
-                mail_pass = '1993323086li'  # 邮箱密码
-                mailAdd = mailInfo['email']  # 外包公司邮箱
+                from_mail = self.user_mail  # 发送邮箱
+                mail_pass = self.user_secret  # 邮箱密码
+                mailAdd = email  # 外包公司邮箱
                 # cc_mail = ['lizhenliang@xxx.com']      # 抄送邮箱
-                from_name = '聚光绘影'  # 发送人姓名
-                subject = '聚光绘影'  # 主题
+                from_name = self.user_name  # 发送人姓名
+                subject = '您有一封来自' + self.user_name + '的邮件'  # 主题
                 mail = [
                     "From: %s <%s>" % (from_name, from_mail),
                     str("To: %s" % mailAdd),
                     "Subject: %s" % subject,
                     # "Cc: %s" % ','.join(cc_mail), "utf8"),
                     "",
-                    "分发test",
+                    self.describe,
                 ]
                 msg = '\n'.join(mail)
-                s = smtplib.SMTP()
-                s.connect(smtp_server, '25')
-                s.login(from_mail, mail_pass)
+                s = smtplib.SMTP('localhost')
+                # s.connect(smtp_server, '25')
+                # s.login(from_mail, mail_pass)
                 # s.sendmail(from_mail, to_mail+cc_mail, msg)
                 s.sendmail(from_mail, mailAdd, msg)
                 s.quit()
-                logging.info(u'发送邮件成功' + mailInfo['name'] + '|'+ mailAdd)
+                logging.info(u'发送邮件成功' + name + '|'+ mailAdd)
             except Exception as e:
                 logging.info('发送邮件失败')
                 logging.error(e)
         else:
             logging.info('邮箱为空')
-        disCallback(['Outsource' + os.sep + mailInfo['name'] + os.sep +
-                     mailInfo['name'] + '_' + self.userid + '_' + self.timeStamp + '.zip', self.task_ids])
+        disCallback(['Outsource' + os.sep + name + os.sep +
+                     name + '_' + self.userid + '_' + self.timeStamp, self.task_ids])
 
     def result(self, req, res):
         res1, res2 = res
