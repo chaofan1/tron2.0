013d45a5e2f63bd847c204c9b2d97001bdbe7ca0
chaofan <wangchaofan1991@gmail.com>
2018-09-14 18:37:27 +0800

    eidt render

--- createDirPath2.0_server/parser.py
+++ createDirPath2.0_server/parser.py
@@ -15,12 +15,11 @@
 #    "Dailies2" "/FUY/001/001/stuff/cmp" "192.168.1.85|FUY/001/001/stuff/cmp|filename|command_id"
 #"Reference" "HAC" "shots"  "192.168.1.85|x:/DHG/References/inner/fileName|373"
 # 剪辑线
-#'clip1' '127.0.0.1|uploads/Projects/xml_files/25/2aa.xml|FUY/001/|7|60|20|44'
+#'clip1' '127.0.0.1|HZG/001/2aa.xml|FUY/001/|7|60|20|44'
 #转码 'clip1' 'IP|user_id|path|项目id|场id|xml_id|command_id'
 #追加 'add_xml' 'IP|xml_path|path|项目id|场id|xml_id|command_id'
 #回插 'clip2' 'IP|video_path|img_path|time|rate|id|command_id'
 #打包 'clip3'  'IP|FUY/001|xml_path|command_id'
-#'127.0.0.1|uploads/Projects/xml_files/20180731/93926b72a9bea849b74e5d51ee72a512.xml|uploads/Projects/FUY/001/|1|002|26'
 
 
 import sys,os
