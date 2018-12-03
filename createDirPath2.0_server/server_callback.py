ddb3e07c19646f6927d8d0dbdd2550acc622252e
lx <1120722567@qq.com>
2018-12-03 15:30:57 +0800

    yun

--- createDirPath2.0_server/server_callback.py
+++ createDirPath2.0_server/server_callback.py
@@ -17,19 +17,19 @@ class CallBack:
     def callback(self, command_id):
         self.url = 'http://%s/tron/index.php/python/renewScriptStatus' % self.ip
         self.query_args = {'id': command_id}
-        self.request()
+        self.request_post()
 
     def callback_pack(self, pack_id):
         self.url = 'http://%s/python/callback_pack_status' % self.ip
         self.query_args = {'id': pack_id}
-        self.request()
+        self.request_get()
 
     def callback_transit(self, transit_id):
         self.url = 'http://%s/python/distribute/callback' % self.ip
         self.query_args = {'command_id': transit_id}
-        self.request()
+        self.request_get()
 
-    def request(self):
+    def request_post(self):
         encoded_args = urllib.urlencode(self.query_args)
         try:
             response = urllib2.urlopen(self.url, encoded_args)
@@ -40,6 +40,19 @@ class CallBack:
         else:
             print response.read()
 
+    def request_get(self):
+        word = urllib.urlencode(self.query_args)
+        newurl = self.url + "?" + word
+        request = urllib2.Request(newurl)
+        try:
+            response = urllib2.urlopen(request)
+        except urllib2.HTTPError, e:
+            print e.code
+        except urllib2.URLError, e:
+            print e.reason
+        else:
+            print response.read()
+
 
 if __name__ == '__main__':
-    CallBack().callback('')
+    CallBack().callback_pack(1)
