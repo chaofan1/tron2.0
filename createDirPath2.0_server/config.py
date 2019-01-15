448a3a210dbe0f2b39457dd560507c4878236312
LX <1120722567@qq.com>
2019-01-15 19:31:58 +0800

    ''

--- createDirPath2.0_server/config.py
+++ createDirPath2.0_server/config.py
@@ -1,5 +1,5 @@
 ALL_WIN = ''
 ALL_MAC = ''
 ALL_LINUX = ''
-log_path_server = '/Public/tronPipelineScript/tron2.0/log/server_log/'
+log_path_server = '/Public/tronPipelineScript/tron2.0/log/'
 outputpath = '/Tron/%s/Vender/outgoing/%s'
